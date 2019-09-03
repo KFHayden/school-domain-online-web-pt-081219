9aaff68814141fd3b8ade8c054c1bb1c51d6b488
Kevin Hayden <kevin.fhayden@gmail.com>
2019-09-03 04:23:01 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -31,7 +31,7 @@ class School
   def sort
     sorted_roster = {}
     @roster.each do |name, grade|
-      sorted_roster[name] == 
+      sorted_roster[name] = grade.sort
   end
   
   school = School.new("Bayside High School")
